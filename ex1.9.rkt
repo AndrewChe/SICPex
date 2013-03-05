b91b6d3bafe9e5b3dfad9d596b29cdb1966c8016
ac <ac@anahoret.net>
2013-03-05 12:40:16 +0200

    1.9 negative arguments now allowed

--- ex1.9.rkt
+++ ex1.9.rkt
@@ -9,8 +9,8 @@
     (if (= a 0)
         b
         (inc (plus (dec a) b))))
-  (plus a b)
-  )
+  (if (> a 0)(plus a b)
+      (- (plus (- a) (- b)))))
 ;; This is recursive process because it's expands 
 ;;and then narrows and all evaluations are stored in memory
 
@@ -19,10 +19,13 @@
     (if (= a 0)
         b
         (plus (dec a) (inc b))))
-  (plus a b))
+  (if (> a 0)(plus a b)
+      (- (plus (- a) (- b)))))
 ;; This is iterative process because each step knows all to continue evaluation
 ;; and it needs fixed value of memory
 
+(iter-plus -3 5)
+(recur-plus -4 2)
 ;; With large arguments it's appears as stopping evaluation in recursive way
 ;; because of memory loading (on my computer with following arguments)
 (iter-plus 5e+6 6e+6)
