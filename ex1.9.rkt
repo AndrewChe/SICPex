53e3dc286b45e6a4719abc63ab7e28b484309894
ac <ac@anahoret.net>
2013-03-05 09:28:52 +0200

    1.9 exercise was added

---
+++ ex1.9.rkt
@@ -0,0 +1,29 @@
+(define (inc x)
+  (+ x 1))
+
+(define (dec x)
+  (- x 1))
+
+(define (recur-plus a b)
+  (define (plus a b)
+    (if (= a 0)
+        b
+        (inc (plus (dec a) b))))
+  (plus a b)
+  )
+;; This is recursive process because it's expands 
+;;and then narrows and all evaluations are stored in memory
+
+(define (iter-plus a b)
+  (define (plus a b)
+    (if (= a 0)
+        b
+        (plus (dec a) (inc b))))
+  (plus a b))
+;; This is iterative process because each step knows all to continue evaluation
+;; and it needs fixed value of memory
+
+;; With large arguments it's appears as stopping evaluation in recursive way
+;; because of memory loading (on my computer with following arguments)
+(iter-plus 5e+6 6e+6)
+(recur-plus 5e+6 6e+6)
